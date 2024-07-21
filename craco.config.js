b4725a1ac2a3f946cad2c0c62a18635aa6a717d3
Vladyslav Olishynskyi <vlad.olishynskyi.work@gmail.com>
2024-07-22 00:25:57 +0300

    misc: basic configuration

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@theme': path.resolve(__dirname, 'src/styles/theme'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@styles/*': path.resolve(__dirname, 'src/styles/*'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@mock': path.resolve(__dirname, 'src/mock'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@navigation': path.resolve(__dirname, 'src/navigation'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+};
